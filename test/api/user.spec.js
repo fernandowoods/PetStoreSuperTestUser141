5a513fd9dcaa21b63b086203d59e653794046f6c
fernandowoods <fewoods16@gmail.com>
2025-08-22 12:17:47 -0300

    Teste completo - erro nos Data Driven forEach

--- test/api/user.spec.js
+++ test/api/user.spec.js
@@ -8,12 +8,12 @@ describe('PetStore Swagger - User', () =>{
 
     // Atributos do grupo/describe
     const request = supertest('https://petstore.swagger.io/v2') // BaseURL
+    const user = require('../../vendors/json/user.json')
     const userPut = require('../../vendors/json/userPut.json')
     const massa1 = require('../../vendors/json/massaUser')
 
     // Funções ou Métodos: Its
     it('POST User', () =>{
-        const user = require('../../vendors/json/user.json')
         return request
             .post('/user')            // endpoint
             .send(user)               // enviar json do user
@@ -35,19 +35,18 @@ describe('PetStore Swagger - User', () =>{
         elemento.phone,
         elemento.userStatus
     ]))
-        ('POST User Data Driven simples %s', (userId, username, firstName, lastName, email, password, phone, userStatus) =>{    // %s para trazer o nome do primeiro campo (se colocasse %s %s traria os dois primeiros campos)
-        const user = require('../../vendors/json/massaUser')
-        user.id = userId        // para substituir o nome que está no json
+        ('POST User Data Driven simples %s', (id, username, firstName, lastName, email, password, phone, userStatus) =>{    // %s para trazer o nome do primeiro campo (se colocasse %s %s traria os dois primeiros campos)
+        user.id = id        // para substituir o nome que está no json
         return request
             .post('/user')            // endpoint
-            .send(massa1)               // enviar json do user
+            .send(user)               // enviar json do user
             .then((res) => {          // o ponto no início significa que é uma continuação do request
                 expect(res.statusCode).toBe(200)
-                expect(res.body.message).toBe(String(userId))
+                expect(res.body.message).toBe(String(id))
                 expect(res.body.type).toBe('unknown')
                 expect(res.body.code).toBe(200)
             })
-    })
+        })
 
     it('GET User', () => {
         return request
@@ -82,10 +81,63 @@ describe('PetStore Swagger - User', () =>{
             .delete(`/user/${username}`)
             .then((res) => {
                 expect(res.statusCode).toBe(200)
-                expect(res.body.message).toBe(userPut.username)
+                expect(res.body.message).toBe(username)
                 expect(res.body.type).toBe('unknown')
                 expect(res.body.code).toBe(200)
             })
     })
+    massa1.array.forEach(({ id, username, firstName, lastName, email, password, phone, userStatus }) => {
+        massa1.id = id        // para substituir o nome que está no json
+            it('POST User', () =>{
+                return request
+                    .post('/user')            // endpoint
+                    .send(user)               // enviar json do user
+                    .then((res) => {          // o ponto no início significa que é uma continuação do request
+                        expect(res.statusCode).toBe(200)
+                        expect(res.body.message).toBe(String(id))
+                        expect(res.body.type).toBe('unknown')
+                        expect(res.body.code).toBe(200)
+                    })
+            })
+
+    })
+
+        it(`GET User Data Driven forEach - ${username}`, () => {
+            return request
+                .get('/user/' + username)      // .get(`/user/${username}`)    moderno template literals
+                .then((res) =>{
+                    expect(res.body.id).toBe(id)
+                    expect(res.body.username).toBe(username)
+                    expect(res.body.firstName).toBe(firstName)
+                    expect(res.body.lastName).toBe(lastName)
+                    expect(res.body.email).toBe(email)
+                    expect(res.body.password).toBe(password)
+                    expect(res.body.phone).toBe(phone)
+                    expect(res.body.userStatus).toBe(userStatus)
+                    expect(res.statusCode).toBe(200)
+                })
+        })
+
+        it(`PUT User Data Driven forEach - ${username}`, () => {
+            return request
+                .put('/user/' + username)
+                .send(userPut)
+                .then((res) =>{
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.message).toBe(String(id))
+                    expect(res.body.type).toBe('unknown')
+                    expect(res.body.code).toBe(200)
+                })
+        })
+
+        it(`DELETE User Data Driven forEach - ${username}`, () => {
+            return request
+                .delete(`/user/${username}`)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.message).toBe(userPut.username)
+                    expect(res.body.type).toBe('unknown')
+                    expect(res.body.code).toBe(200)
+                })
+        })
 })
-    
